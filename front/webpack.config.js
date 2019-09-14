243dbed1f05477586af144fbb7cd28f3435d9fbe
c10t <c10t@fa.ke>
2019-09-14 20:24:45 +0900

    setup webpack+typescript+react

---
+++ front/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const rules = [{
+  test: /\.tsx?/,
+  exclude: /node_modules/,
+  loader: 'babel-loader'
+}]
+
+module.exports = {
+  target: 'web',
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  devServer: {
+    contentBase: __dirname,
+    port: 4989
+  }
+}
